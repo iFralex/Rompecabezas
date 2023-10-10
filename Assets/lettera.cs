da8c544c4a78a293b4dda5a5b48ff1f1a1312fb5
iFralex <61825057+iFralex@users.noreply.github.com>
2023-10-10 07:10:43 +0200

    Asset files

---
+++ Assets/lettera.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class lettera : MonoBehaviour
+{
+    bool torna;
+    public Vector2 pos;
+    public GraphicRaycaster m_Raycaster;
+    public PointerEventData m_PointerEventData;
+    public EventSystem m_EventSystem;
+
+    void Update()
+    {
+        if (torna)
+        {
+            transform.position = Vector2.Lerp(transform.position, pos, Time.deltaTime * 5);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -1);
+        }
+    }
+
+    public void Trascina()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void TornaIndietro(bool a)
+    {
+        torna = a;
+        if (!a)
+        {
+            GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+        }
+        else
+        {
+            GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
+        }
+        transform.SetAsLastSibling();
+        m_PointerEventData = new PointerEventData(m_EventSystem);
+        m_PointerEventData.position = Input.mousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+        m_Raycaster.Raycast(m_PointerEventData, results);
+        if (results.Count > 2)
+        {
+            if (results[2].gameObject.name == name)
+            {
+                results[2].gameObject.transform.GetChild(0).GetComponent<Text>().text = GetComponent<Text>().text;
+                results[2].gameObject.transform.GetChild(0).GetComponent<Text>().color = Color.black;
+                if (results[2].gameObject.name == "Cuerpo")
+                {
+                    Destroy(results[2].gameObject.transform.GetChild(1).gameObject);
+                }
+                torna = false;
+                transform.parent.parent.GetComponent<AudioSource>().clip = transform.parent.parent.GetComponent<organizzaLettera>().clipSuc;
+                transform.parent.parent.GetComponent<AudioSource>().Play();
+                transform.parent.parent.GetComponent<organizzaLettera>().completi++;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/organizzaLettera.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class organizzaLettera : MonoBehaviour
+{
+    public List<string> testi, testi1;
+    public List<Text> pezzi, titoli;
+    public AudioClip clipSuc, clipTras;
+    public int completi, scena;
+    float tempo, posX;
+    public RuntimeAnimatorController animazione;
+    public List<RectTransform> posY;
+
+    void Awake()
+    {
+        int i = Screen.currentResolution.width;
+        Screen.SetResolution(i, i * 1080 / 1920, FullScreenMode.Windowed, 60);
+    }
+
+    void Start()
+    {
+        posX = pezzi[0].transform.position.x;
+        int j = Random.Range(3, 8);
+        pezzi[2].rectTransform.position = new Vector2(posX, posY[j].position.y);
+        pezzi[2].GetComponent<lettera>().pos = pezzi[2].transform.position;
+        for (int i = 3; i >= -3; i--)
+        {
+            posY.RemoveAt(j + i);
+        }
+
+        pezzi.RemoveAt(2);
+        for (int i = 0; i < pezzi.Count; i++)
+        {
+            int o = Random.Range(0, posY.Count);
+            pezzi[i].transform.position = new Vector2(posX, posY[o].position.y);
+            pezzi[i].GetComponent<lettera>().pos = pezzi[i].transform.position;
+            posY.RemoveAt(o);
+        }
+        completi = -1;
+    }
+
+    void Update()
+    {
+        if (completi == 5)
+        {
+            transform.GetChild(0).position = new Vector2(Mathf.Lerp(transform.GetChild(0).position.x, Screen.width/2, Time.deltaTime * 3), transform.GetChild(0).position.y);
+            if (tempo == 0)
+            {
+                transform.GetChild(4).gameObject.SetActive(false);
+                transform.GetChild(5).gameObject.SetActive(false);
+            }
+            tempo += Time.deltaTime;
+            if (tempo >= 2)
+            {
+                transform.GetChild(8).gameObject.SetActive(true);
+                GetComponent<Animator>().runtimeAnimatorController = animazione;
+                completi = 6;
+                tempo = 1;
+            }
+        }
+
+        if (completi >= 7)
+        {
+            tempo += Time.deltaTime;
+            if (tempo >= 3.1f)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            }
+        }
+
+        if (completi == -1)
+        {
+            tempo += Time.deltaTime;
+            if (tempo > 2)
+            {
+                tempo = 0;
+                completi = 0;
+                GetComponent<Animator>().runtimeAnimatorController = null;
+            }
+        }
+    }
+
+    public void Avanti()
+    {
+        completi++;
+        GetComponent<Animator>().SetTrigger("vai");
+    }
+}
